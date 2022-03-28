758d4b59731c72430511797dab2d41a5b318dff1
LAPTOP-B0DFG8D0\Pzhui <pangzhihui1220@gmail.com>
2022-03-28 12:40:33 +0800

    DataFetching

---
+++ datafetching/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import DataFetchingTwo from './component/DataFecthingTwo';
+import DataFetchingOne from './component/DataFetchingOne';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <DataFetchingOne /> */}
+      <DataFetchingTwo />
+    </div>
+  );
+}
+
+export default App;
